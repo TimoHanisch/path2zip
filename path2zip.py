0fe65d7d5e8ee1322b1fb093cd70e7fe7209a874
Timo Hanisch <timohanisch@gmail.com>
2015-03-30 01:31:20 +0200

    Initial commit

---
+++ path2zip.py
@@ -0,0 +1,32 @@
+__author__ = 'Timo Hanisch'
+
+import sys
+import os
+import zipfile
+import re
+
+
+def extract_csv(path, zip_file, regex):
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if re.match(regex, f):
+                root_path = os.path.join(root, f)
+                rel_path = os.path.relpath(root_path, path)
+                zip_file.write(os.path.join(root, f), rel_path)
+
+
+def main(path, zip_file_name, regex='.*'):
+    zip_file = zipfile.ZipFile(zip_file_name + '.zip', 'w')
+    extract_csv(path, zip_file, regex)
+    zip_file.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'Usage of csv2zip: path2zip "<path>" "<name>" ["<regex>"]'
+        print '\t<path>: The path of the root folder to zip'
+        print '\t<name>: The name of the zip file to create'
+        print '\t<regex> (optional): A regex which is matched against all files found in the path\n' \
+              '\tand its sub folders. By default all files (.*) are written to the zip file'
+        sys.exit(-1)
+    sys.exit(main(sys.argv[1], sys.argv[2], sys.argv[3]))
